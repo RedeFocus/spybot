f52ff92fc862daa8459b8cb7f23fe2a9d97c4ba3
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2018-12-27 18:50:43 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -135,7 +135,7 @@ ID's bloqueadas: Nenhuma ID punida. - `)
         });
         client.on("ready", () => {
 
-            let img = ["https://images-ext-2.discordapp.net/external/EmNvRIbCjvm6wyNr0lShpky1Q8UYxbJq0HP7KKmlfnM/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/513375539277594625/d876809f2cdee81842e06e4e82aade62.png?width=80&height=80"];
+           let img = ["https://images-ext-2.discordapp.net/external/EmNvRIbCjvm6wyNr0lShpky1Q8UYxbJq0HP7KKmlfnM/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/513375539277594625/d876809f2cdee81842e06e4e82aade62.png?width=80&height=80" ];
             
               function setImg() {
                   let randomImage = img[Math.floor(Math.random() * img.length)];
