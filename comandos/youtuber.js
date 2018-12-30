1240347e4e2a4ae2fd163ffdb5e9b8e077a83bfa
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2018-12-30 01:44:58 -0200

    Update youtuber.js

--- comandos/youtuber.js
+++ comandos/youtuber.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
-    var ids = ["341046919025524746", "2", "3"];
-if (ids.includes (message.author.id)) return message.channel.send("você está proibido de me usar!")
 message.delete();
 
 const embed = new Discord.RichEmbed()
