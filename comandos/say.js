7f2be19fcf7b19ec2e45a19df97cbb20f7930937
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2018-12-30 13:37:00 -0200

    Update say.js

--- comandos/say.js
+++ comandos/say.js
@@ -2,8 +2,7 @@ exports.run = async (client, message, args) => {
   var ids = ["341046919025524746", "503398173524623360", "3"];
 if (ids.includes (message.author.id)) return message.channel.send("você está proibido de usar esse comando.")
       
-
-    
+if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply("você não tem permissões!");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{});  
     message.channel.send(sayMessage);
