1de7fd126a6f19e6b219d35b058ddacf584799a5
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2018-12-30 13:35:55 -0200

    Update anunciar.js

--- comandos/anunciar.js
+++ comandos/anunciar.js
@@ -7,7 +7,7 @@ if (ids.includes (message.author.id)) return message.channel.send("você está p
     
   
     let target = message.author
-    if(message.member.hasPermission("MANAGE_ROLES")) {
+   if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("você não tem permissões!");
         const color = args[1]
         const text = args.slice(1).join(" ");
         if (text.length < 1) return message.channel.send("Você não colocou a mensagem que vai ser anunciada!");
