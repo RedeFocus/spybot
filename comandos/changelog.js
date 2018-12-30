1cd9417074c8337d330090392001c3ddf06f02a6
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2018-12-30 01:28:29 -0200

    Update changelog.js

--- comandos/changelog.js
+++ comandos/changelog.js
@@ -10,11 +10,12 @@ if (ids.includes (message.author.id)) return message.channel.send("você está p
     if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply("você não tem permissões!");
         const color = args[1]
         const text = args.slice(1).join(" ");
-        if (text.length < 1) return message.channel.send("Você não colocou a mensagem que vai ser anunciada!");
+        if (text.length < 1) return message.channel.send("Você tem que usar da forma: ``p-changelog EQUIPE <msg>");
         const embed = new Discord.RichEmbed()
         .setColor(11543845)
-        .setTitle("\:barber: Changelog atualizada! \:barber: ")
-        .setDescription(text)
+        .setTitle("\:barber: Changelog:  \:barber: ")
+        
+        .setDescription("▫", text)
         .setFooter(`ChangeLog atualizada por: ${target.username}`, message.author.displayAvatarURL)
         .setTimestamp()
         .setThumbnail("http://www.workschedule.net/wp-content/uploads/2014/08/changelog1.png")
