c4ab0020167e2f879b58c6ff03658f7ef5c35c7f
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2018-12-31 03:17:23 -0200

    Update changelog.js

--- comandos/changelog.js
+++ comandos/changelog.js
@@ -10,7 +10,7 @@ if (ids.includes (message.author.id)) return message.channel.send("você está p
     if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply("você não tem permissões!");
         const color = args[1]
         const text = args.slice(1).join(" ");
-        if (text.length < 1) return message.channel.send("Você tem que usar da forma: ``p-changelog EQUIPE <msg>");
+        if (text.length < 1) return message.channel.send("Você tem que usar da forma: ``p-changelog -e <msg>``");
         const embed = new Discord.RichEmbed()
         .setColor(11543845)
         .setTitle("\:barber: **CHANGELOG** \:barber: ")
@@ -20,7 +20,6 @@ if (ids.includes (message.author.id)) return message.channel.send("você está p
         .setFooter(`| Hellen | ${target.username}`, message.author.displayAvatarURL)
         .setTimestamp()
         .setThumbnail("http://www.workschedule.net/wp-content/uploads/2014/08/changelog1.png")
-        message.channel.send("@everyone @here")
         message.channel.send({embed})
 
   }
