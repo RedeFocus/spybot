c5f2c8fffdcd0ad7c76964be61728fde51991eff
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2019-01-04 22:51:15 -0200

    Add files via upload

---
+++ listanegra.js
@@ -0,0 +1,10 @@
+exports.run = async (client, message, args) => {
+    var ids = ["341046919025524746", "503398173524623360", "3"];
+  if (ids.includes (message.author.id)) return message.channel.send("você está proibido de usar esse comando.")
+        
+  
+      
+      const sayMessage = args.join(" ");
+      message.delete().catch(O_o=>{});  
+      message.channel.send(lista-negra);
+    }
