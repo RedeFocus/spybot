0ae5cf6f332bcd3f0f69b89162a031833ce6ef73
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2019-01-05 22:03:23 -0200

    Update FalsaMensagem.js

--- comandos/FalsaMensagem.js
+++ comandos/FalsaMensagem.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args) => {
     var ids = ["341046919025524746", "2", "3"];
-if (ids.includes (message.author.id)) return message.channel.send("você está proibido de me usar!")
+if (ids.includes (message.author.id)) return message.channel.send("você está proibido de me usar! HAHAHHA")
       
     try {
 let user;
