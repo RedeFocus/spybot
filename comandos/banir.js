a2068ee478cf582d1866f3feb230657a8daf5608
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2019-01-07 19:42:24 -0200

    Update banir.js

--- comandos/banir.js
+++ comandos/banir.js
@@ -6,8 +6,7 @@ module.exports.run = async(bot, message, args) => {
   
     let user = message.mentions.users.first();
     let reason = message.content.split(" ").slice(2).join(" ");
-    if (message.mentions.users.size < 1) return message.reply("mencione algum usuário para que ele possa ser punido.");
-    if (!message.guild.member(user).bannable) return message.reply("o usuário mencionado possui um cargo superior ao meu.");
+   
     if (message.author.id === user.id) return message.reply("eu não posso te banir, haha.")
     message.reply(`você está prestes a banir ${user}. Para confirmar a operação, clique no emoji abaixo.`).then(msg => {
         msg.react('✅')
