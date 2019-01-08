b3023929e41a4e28c946d100b4507397459305fc
PedroMzika <46200448+PedroMzika@users.noreply.github.com>
2019-01-08 18:30:33 -0200

    Update play.js

--- comandos/play.js
+++ comandos/play.js
@@ -2,7 +2,7 @@ const ytdl = require('ytdl-core');
 
 exports.run = async (client, message, args, ops) => {
   var ids = ["341046919025524746", "2", "3"];
-if (ids.includes (message.author.id)) return message.channel.send("você está proibido de me usar!")
+if (ids.includes (message.author.id)) return message.channel.send("você está proibido de me usar! kekekekek")
       
   message.delete()
   let Canalvoz = message.member.voiceChannel;
